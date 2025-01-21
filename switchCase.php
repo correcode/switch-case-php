2af8c6d7215609974a71776348b1d1e03f17802e
Alisson Correa Alves <aliba.truta@gmail.com>
2025-01-21 18:10:58 -0300

    Add files via upload

---
+++ switchCase.php
@@ -0,0 +1,12 @@
+<?php
+
+switch ($idade = 30) {
+    case 30:
+        echo "Voce pode entrar!\n";
+            break;
+    case 15:
+        echo "Voce precisa de permissao!\n";
+            break;
+    default:
+        echo "Voce nao pode entrar!\n";
+}
